cd79f774013acc3061975e57458cab0b489e4e71
Hinterix <76217862+ErythroCraft@users.noreply.github.com>
2026-06-02 13:44:36 +0200

    Save and clear player inventory on dimension entry
    Save the player's inventory when entering the Builders Dimension and clear it.

--- src/main/java/de/erythrocraft/buildersdimension/procedures/BuildersDimensionPlayerEntersDimensionProcedure.java
+++ src/main/java/de/erythrocraft/buildersdimension/procedures/BuildersDimensionPlayerEntersDimensionProcedure.java
@@ -1,14 +1,23 @@
 package de.erythrocraft.buildersdimension.procedures;
 
-import net.minecraft.world.level.GameType;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.GameType;
 
 public class BuildersDimensionPlayerEntersDimensionProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof ServerPlayer _player)
+		if (entity instanceof ServerPlayer _player) {
+			ListTag survivalInv = new ListTag();
+			_player.getInventory().save(survivalInv);
+
+			_player.getPersistentData().put("BuildersDimensionSurvivalInv", survivalInv);
+
+			_player.getInventory().clearContent();
+
 			_player.setGameMode(GameType.CREATIVE);
+		}
 	}
 }
