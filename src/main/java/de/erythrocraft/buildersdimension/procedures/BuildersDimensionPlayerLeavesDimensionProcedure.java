e6b6f0474cebcafc0f89fcb84ba1dd1bada62e72
Hinterix <76217862+ErythroCraft@users.noreply.github.com>
2026-06-02 13:45:14 +0200

    Handle player leaving Builders Dimension
    Clear player's inventory and restore original inventory from persistent data when leaving the Builders Dimension.

--- src/main/java/de/erythrocraft/buildersdimension/procedures/BuildersDimensionPlayerLeavesDimensionProcedure.java
+++ src/main/java/de/erythrocraft/buildersdimension/procedures/BuildersDimensionPlayerLeavesDimensionProcedure.java
@@ -1,14 +1,25 @@
 package de.erythrocraft.buildersdimension.procedures;
 
-import net.minecraft.world.level.GameType;
-import net.minecraft.world.entity.Entity;
+import net.minecraft.nbt.ListTag;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.level.GameType;
 
 public class BuildersDimensionPlayerLeavesDimensionProcedure {
 	public static void execute(Entity entity) {
 		if (entity == null)
 			return;
-		if (entity instanceof ServerPlayer _player)
+		if (entity instanceof ServerPlayer _player) {
+			_player.getInventory().clearContent();
+
+			if (_player.getPersistentData().contains("BuildersDimensionSurvivalInv", 9)) {
+				ListTag originalInv = _player.getPersistentData().getList("BuildersDimensionSurvivalInv", 10);
+				_player.getInventory().load(originalInv);
+
+				_player.getPersistentData().remove("BuildersDimensionSurvivalInv");
+			}
+
 			_player.setGameMode(GameType.SURVIVAL);
+		}
 	}
 }
